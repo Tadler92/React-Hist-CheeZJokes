6a5173fe1a54133daaddcb0f119cc38854fe1220
Trevor Duhon <taduhon1485@gmail.com>
2024-05-21 23:47:28 -0500

    Refactored 'App' Component to be a function rather than a class component

--- src/App.jsx
+++ src/App.jsx
@@ -41,14 +41,27 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <JokeList />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+const App = () => {
+  // render() {
     return (
       <div className="App">
         <JokeList />
       </div>
     );
-  }
+  // }
 }
 
 export default App;
