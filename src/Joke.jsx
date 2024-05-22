a586fb13170709d9e733d0033fe470419891aa02
Trevor Duhon <taduhon1485@gmail.com>
2024-05-21 22:06:42 -0500

    Refactored 'Joke' Component to be a function rather than a class component

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -3,18 +3,45 @@ import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
+// class Joke extends Component {
+//   render() {
+//     const { id, vote, votes, text } = this.props;
+
+//     return (
+//       <div className="Joke">
+//         <div className="Joke-votearea">
+//           <button onClick={evt => vote(id, +1)}>
+//             <i className="fas fa-thumbs-up" />
+//           </button>
+
+//           <button onClick={evt => vote(id, -1)}>
+//             <i className="fas fa-thumbs-down" />
+//           </button>
+
+//           {votes}
+//         </div>
+
+//         <div className="Joke-text">{text}</div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Joke;
+
+
+const Joke = ({ id, vote, votes, text }) => {
+  // render() {
+    // const { id, vote, votes, text } = this.props;
 
     return (
       <div className="Joke">
         <div className="Joke-votearea">
-          <button onClick={evt => vote(id, +1)}>
+          <button onClick={() => vote(id, +1)}>
             <i className="fas fa-thumbs-up" />
           </button>
 
-          <button onClick={evt => vote(id, -1)}>
+          <button onClick={() => vote(id, -1)}>
             <i className="fas fa-thumbs-down" />
           </button>
 
@@ -24,7 +51,7 @@ class Joke extends Component {
         <div className="Joke-text">{text}</div>
       </div>
     );
-  }
+  // }
 }
 
 export default Joke;
